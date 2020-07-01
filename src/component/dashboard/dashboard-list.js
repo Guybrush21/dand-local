b9c02395a0c31278a7322d36093c2dc61c8e55d9
Nicholas Pellizer <nicholasp21@gmail.com>
2020-07-01 13:15:41 +0200

    fix cards

--- src/component/dashboard/dashboard-list.js
+++ src/component/dashboard/dashboard-list.js
@@ -8,7 +8,7 @@ export default function DashboardList(props){
     const list = props.elements?.map(c => (
         <Card interactive={true} key={c._id}
           //onClick={(e) => this.props.onSelect(c, e)}
-          className='flex-card'>                
+          className='dashboard-card'>                
           <h3>{c[props.title]}</h3>                    
           { (c[imageUrl]) ?  <img src={c[imageUrl]} alt="item" 
           className="dasboard-list-image"></img>  : '' }

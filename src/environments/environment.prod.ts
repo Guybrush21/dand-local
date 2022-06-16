cd406824f971a0a8d48accec55dbc438cba06ea0
Nicholas Pellizer <nicholasp21@gmail.com>
2022-06-16 23:41:28 +0200

    Fix production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyCnRzaQdA0eTkKG2MPHGRZn54wCUwLeDvI',
+    authDomain: 'dand-1d995.firebaseapp.com',
+    projectId: 'dand-1d995',
+    storageBucket: 'dand-1d995.appspot.com',
+    messagingSenderId: '1001829167017',
+    appId: '1:1001829167017:web:ff59c8bf9952ec4658deb8',
+  },
 };

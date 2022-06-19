a92c365b936d58943f4f391f054baecfa2ec3208
Nicholas Pellizer <nicholasp21@gmail.com>
2022-06-19 16:13:38 +0200

    Diary log db implementation and tests

--- src/app/db/db.service.spec.ts
+++ src/app/db/db.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { CHARACTER_TYPE } from '../common/constant';
+import { CHARACTER_TYPE, LOG_RECORD_TYPE } from '../common/constant';
 import Character from '../model/character.model';
 import PouchDB from 'pouchdb';
 import { v4 } from 'uuid';
 import { DbService } from './db.service';
 import { removeCharacter } from '../state/character/character.action';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import LogRecord from '../model/logRecord.model';
 
 describe('DbService', () => {
   let service: DbService;
@@ -35,7 +36,6 @@ describe('DbService', () => {
   it('should save new character', async () => {
     const character: Character = {
       _id: null,
-      _rev: null,
       name: 'nicholas',
       class: 'developer',
       isFavorite: false,
@@ -75,5 +75,37 @@ describe('DbService', () => {
     const removeResult = await service.removeCharacter(c);
 
     expect(removeResult).toBeTrue();
+  });
+
+  it('should add a logrecord', async () => {
+    const record: LogRecord = {
+      isFavorite: false,
+      message: 'testing message',
+      type: LOG_RECORD_TYPE,
+    };
+    const result = await service.saveLogRecord(record);
+    expect(result).toBeDefined();
+    expect(result.timestamp).toBeDefined();
+    expect(result.number).toBeDefined();
+  });
+
+  it('log number should auto-increase', async () => {
+    const record: LogRecord = {
+      isFavorite: false,
+      message: 'testing message 1',
+      type: LOG_RECORD_TYPE,
+    };
+    const result1 = await service.saveLogRecord(record);
+
+    const result2 = await service.saveLogRecord({
+      ...record,
+      message: 'testing message 2',
+    });
+    console.log(result1);
+    console.log(result2);
+
+    expect(result1.number).toBeDefined();
+    expect(result2.number).toBeDefined();
+    expect(result1.number).toBeLessThan(result2.number);
   });
 });
